b5534931843c63c5309b4a64906f2b5830d62324
Hasham Nadeem <hashamnadeem876@gmail.com>
2025-03-19 09:01:40 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 echo "Running tests..."
-# TODO: Add commands to run unit and integration tests
+
+# Run unit tests
+echo "Running unit tests..."
+pytest tests/unit/ --maxfail=1 --disable-warnings -q  # Specify the path to unit tests
+
+# Check the result of unit tests
+if [ $? -ne 0 ]; then
+    echo "Unit tests failed!"
+    exit 1
+fi
+
+# Run integration tests
+echo "Running integration tests..."
+pytest tests/integration/ --maxfail=1 --disable-warnings -q  # Specify the path to integration tests
+
+# Check the result of integration tests
+if [ $? -ne 0 ]; then
+    echo "Integration tests failed!"
+    exit 1
+fi
+
+echo "All tests passed!"
