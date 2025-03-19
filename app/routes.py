34e33072c33f79aecc7e7f39a805c71616bb1f34
Hasham Nadeem <hashamnadeem876@gmail.com>
2025-03-19 08:48:42 +0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
 from app.models import User
+from datetime import datetime
 
 @app.route('/')
 def index():
@@ -12,7 +13,12 @@ def submit():
     email = request.form['email']
     phone = request.form.get('phone', '')  # Optional field
     adress = request.form.get('adress', '')  # Optional field
-    user = User(name=name, email=email, phone=phone, adress=adress)
+    date_of_birth_str = request.form.get('date_of_birth')  # Get date of birth from the form
+
+    # Convert the date string to a date object
+    date_of_birth = datetime.strptime(date_of_birth_str, '%Y-%m-%d') if date_of_birth_str else None
+
+    user = User(name=name, email=email, phone=phone, adress=adress, date_of_birth=date_of_birth)
     db.session.add(user)
     db.session.commit()
     return redirect(url_for('index'))
